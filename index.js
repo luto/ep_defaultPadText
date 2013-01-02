a00cae274d144a5143c44158089889db9a45b800
mluto <m@luto.at>
2013-01-02 15:33:31 +0100

    Load prefixes and texts from settings

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-exports.padCreate = function(hook, context){
+var settings = require('ep_etherpad-lite/node/utils/Settings');
+
+/*
+"ep_defaultPadText" : {
+                   "defaultText" : {
+                                     "prefix" : "mm",
+                                     "text"   : "asdsad numerb:$num$ date:$date$"
+                                   }
+                 }
+*/
+
+exports.padCreate = function(hook, context)
+{
 	var id = context.pad.id;
-	var parts = id.split('_');
-	var prefix = parts[0];
-	var number = parts[1];
 	
-	console.log("got " + prefix);
-	console.log("context: ");
-	console.log(context);
-	
-	switch(prefix)
+	for(show in settings.ep_defaultPadText)
 	{
-		case 'mm':
-		context.pad.setText("It's MobileMacs, number: " + number)
-		break;
-		case 'cre':
-		context.pad.setText("It's CRE, number: " + number)
-		break;
+		if(id.indexOf(show) == 0)
+		{
+			console.log("It is: " + show.prefix)
+			var text = settings.ep_defaultPadText[show].text;
+			text = prepareText(text, id);
+			context.pad.setText(text);
+		}
 	}
+}
+
+function prepareText(text, number)
+{
+	// TODO
+	return text;
 }
