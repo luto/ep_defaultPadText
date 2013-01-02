069ff4261f458f22e1a3ca21d6bf9839d96ec00f
mluto <m@luto.at>
2013-01-02 18:55:26 +0100

    Added README, formatting according to ep-lite-contributing-guidelines

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-ep_defaultPadText
-=================
+# ep_defaultPadText
+
+## Function
+ep_defaultPadText let's you choose a special default text for each pad-prefix. This can be be used for pads that all share the same header, for example collaborative meeting-notes could all look like like this:
+```
+# Weekly-Meeting
+Date and Time: April 28, 2011 18:21:08
+
+## Agenda
+## Conclusions
+## Roadmap for next week
+```
+Using this ep_defaultPadText you don't have to copy&paste that into your pad yourself, you just choose an prefix for meetings, for example `m_`, paste the template int your `settings.json` and you're good to go.
+
+## Settings
+
+### Format
+You need to add the following section to your `settings.json`. You can repeat the inner
+sections as often as you want. The name of the inner rections represents the prefix,
+the `text`-attribute is the text that will be set, it can contain placeholders, which
+will be explained in the next paragraph.
+
+```JSON
+"ep_defaultPadText" : {
+                        "mm" : {
+  			                     "text" : "MobileMacs-Episode: $num$ on $date:%B %d, %y$"
+                               },
+                        "wr" : {
+  			                     "text" : "WRINT-Time! Number $num$ happening in your home-town at $date:%H:%M:%S$"
+                               }
+                      }
+```
+
+### Placeholders
+  * `$num$`, number after the given prefix; For example: if the Pad-ID is `mm42`, `$num$` will be `42`
+  * `$date:format$`, the date will be formatted by [strftime](https://github.com/samsonjs/strftime)
--- index.js
+++ index.js
@@ -1,39 +1,32 @@
 var settings = require('ep_etherpad-lite/node/utils/Settings');
 var strftime = require('strftime');
 
-/*
-"ep_defaultPadText" : {
-                        "mm" : {
-				               "text"   : "asdsad numerb:$num$ date:$date:%y$"
-                               }
-                      },
-
-*/
-
 exports.padCreate = function(hook, context)
 {
-	var id = context.pad.id;
-	
-	for(show in settings.ep_defaultPadText)
-	{
-		if(id.indexOf(show) == 0)
-		{
-			var number = id.substring(show.length);
-			var text = settings.ep_defaultPadText[show].text;
-			text = prepareText(text, number);
-			context.pad.setText(text);
-		}
-	}
+  var id = context.pad.id;
+  
+  for(show in settings.ep_defaultPadText)
+  {
+    if(id.indexOf(show) == 0)
+    {
+      var number = id.substring(show.length);
+      var text = settings.ep_defaultPadText[show].text;
+      text = prepareText(text, number);
+      context.pad.setText(text);
+    }
+  }
 }
 
 function prepareText(text, number)
 {
-	console.log("prepare: " + text  + "   " + number);
-	text = text.replace("$num$", number);
-	
-	var dateTokenRegex = /\$date:([^$]+)\$/;
-	var dateToken = dateTokenRegex.exec(text);
-	text = text.replace(dateToken[0], strftime(dateToken[1]));
-	
-	return text;
+  console.log("prepare: " + text  + "   " + number);
+  text = text.replace("$num$", number);
+  
+  // Extract the $date$-placeholder out of our template
+  var dateTokenRegex = /\$date:([^$]+)\$/;
+  var dateToken = dateTokenRegex.exec(text);
+  // replace the full placeholder by the current date formatted using the given format
+  text = text.replace(dateToken[0], strftime(dateToken[1]));
+  
+  return text;
 }
