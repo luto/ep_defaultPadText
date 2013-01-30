69243477898431a99f5b026110a878378c221c32
Kambfhase <kambfhase@hotmail.de>
2013-01-30 21:23:07 +0100

    Update index.js
    The code has been beautified to fit the node.js coding style guide. More importantly it does no longer leak the `show` variable.

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
 var settings = require('ep_etherpad-lite/node/utils/Settings');
 var strftime = require('strftime');
 
-exports.padCreate = function(hook, context)
-{
-  var id = context.pad.id;
+function prepareText(text, number) {
+    var dateTokenRegex = /\$date:([^$]+)\$/, dateToken;
   
-  for(show in settings.ep_defaultPadText)
-  {
-    if(id.indexOf(show) == 0)
-    {
-      var number = id.substring(show.length);
-      var text = settings.ep_defaultPadText[show].text;
-      text = prepareText(text, number);
-      context.pad.setText(text);
-    }
-  }
+    console.log("prepare: " + text + "   " + number);
+    text = text.replace("$num$", number);
+
+    // Extract the $date$-placeholder out of our template
+    dateToken = dateTokenRegex.exec(text);
+    
+    // replace the full placeholder by the current date formatted using the given format
+    text = text.replace(dateToken[0], strftime(dateToken[1]));
+
+    return text;
 }
 
-function prepareText(text, number)
-{
-  console.log("prepare: " + text  + "   " + number);
-  text = text.replace("$num$", number);
-  
-  // Extract the $date$-placeholder out of our template
-  var dateTokenRegex = /\$date:([^$]+)\$/;
-  var dateToken = dateTokenRegex.exec(text);
-  // replace the full placeholder by the current date formatted using the given format
-  text = text.replace(dateToken[0], strftime(dateToken[1]));
-  
-  return text;
-}
+exports.padCreate = function (hook, context) {
+    var id = context.pad.id, show, number, text;
+
+    for (show in settings.ep_defaultPadText) {
+        if (id.indexOf(show) === 0) {
+            number = id.substring(show.length);
+            text = settings.ep_defaultPadText[show].text;
+            text = prepareText(text, number);
+            context.pad.setText(text);
+        }
+    }
+};
+
