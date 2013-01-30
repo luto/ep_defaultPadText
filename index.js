775be57ad8f26f854ebdd05159fe4f6eadc310e9
Kambfhase <kambfhase@hotmail.de>
2013-01-30 21:31:30 +0100

    Format according to EPL

--- index.js
+++ index.js
@@ -2,30 +2,31 @@ var settings = require('ep_etherpad-lite/node/utils/Settings');
 var strftime = require('strftime');
 
 function prepareText(text, number) {
-    var dateTokenRegex = /\$date:([^$]+)\$/, dateToken;
-  
-    console.log("prepare: " + text + "   " + number);
-    text = text.replace("$num$", number);
+  var dateTokenRegex = /\$date:([^$]+)\$/,
+    dateToken;
 
-    // Extract the $date$-placeholder out of our template
-    dateToken = dateTokenRegex.exec(text);
-    
-    // replace the full placeholder by the current date formatted using the given format
-    text = text.replace(dateToken[0], strftime(dateToken[1]));
+  console.log("prepare: " + text + "   " + number);
+  text = text.replace("$num$", number);
 
-    return text;
+  // Extract the $date$-placeholder out of our template
+  dateToken = dateTokenRegex.exec(text);
+
+  // replace the full placeholder by the current date formatted using the given format
+  text = text.replace(dateToken[0], strftime(dateToken[1]));
+
+  return text;
 }
 
 exports.padCreate = function (hook, context) {
-    var id = context.pad.id, show, number, text;
+  var id = context.pad.id,
+    show, number, text;
 
-    for (show in settings.ep_defaultPadText) {
-        if (id.indexOf(show) === 0) {
-            number = id.substring(show.length);
-            text = settings.ep_defaultPadText[show].text;
-            text = prepareText(text, number);
-            context.pad.setText(text);
-        }
+  for (show in settings.ep_defaultPadText) {
+    if (id.indexOf(show) === 0) {
+      number = id.substring(show.length);
+      text = settings.ep_defaultPadText[show].text;
+      text = prepareText(text, number);
+      context.pad.setText(text);
     }
+  }
 };
-
